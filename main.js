c084a6571f6be8d953462886125ee54b4cef204d
Paulo <ribeiro@gamil.com>
2022-03-15 21:19:38 -0400

    atualizar

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-
-
 const $ = document.querySelector.bind(document)
+const loadTasks = () =>{
+const tasksStr = localStorage.getItem('tasks')
+if(tasksStr) {
+  const tasks = JSON.parse(tasksStr)
+  tasks.foreEach((t)=>{
+    const newTaskItem = `
+    <li>${t} </li>
+    `
+    $('#task-list').insertAdjacentHTML('beforrend', newTaskItem)
+  })
+}
+}
+
+loadTasks()
 
 const addTask = (event) => {
   event.preventDefault()
@@ -10,7 +22,19 @@ const addTask = (event) => {
   const newTaskItem = `
   <li>${taskDescription} </li>
   `
-
   $('#task-list').insertAdjacentHTML('beforeend', newTaskItem)
   $('#task-form').reset()
+  savetask(taskDescription)
+}
+
+const savetask = (task) => {
+  let tasksStr = localStorage.getItem('tasks')
+  let tasks = []
+  if (tasksStr) {
+    tasks = JSON.parse(tasksStr)
+  }
+  tasks.push(task)
+  tasksStr = JSON.stringify(tasks)
+  localStorage.setItem('tasks', tasksStr)
+
 }
